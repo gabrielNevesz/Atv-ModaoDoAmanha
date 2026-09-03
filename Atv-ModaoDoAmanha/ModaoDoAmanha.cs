c48e4232d7d9a56dc3852774606e96ea1e482eea
Gabriel Fuchelberguer Neves <gabrielfuchelberguer@gmail.com>
2026-09-03 17:03:02 -0300

    feat: Codificando projeto

---
+++ Atv-ModaoDoAmanha/ModaoDoAmanha.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atv_ModaoDoAmanha
+{
+    internal class AlunoMusica
+    {
+        public string Nome { get; set; } = string.Empty;
+        public double MensalidadeBase { get; set; }
+        public double HorasPraticaSemanal { get; set; }
+        public int NivelHabilidade { get; set; }
+
+
+        public double DescontoHoras()
+        {
+            bool descontoMensalidade = HorasPraticaSemanal > 10.0;
+            return descontoMensalidade ? (HorasPraticaSemanal * 0.15) - MensalidadeBase : MensalidadeBase;
+        }
+
+        public double HorasFaltantesProximoNivel()
+        {
+            double Nivel1 = NivelHabilidade * 60;
+            double Nivel2 = NivelHabilidade * 80;
+            double Nivel3 = NivelHabilidade * 85;
+            double Nivel4 = NivelHabilidade * 90;
+            double Nivel5 = NivelHabilidade * 100;
+            return  HorasPraticaSemanal * 4;
+        }
+
+        public double MensalidadeFinal()
+        {
+            double valorFinal = DescontoHoras() / MensalidadeBase;
+            return valorFinal;
+        }
+
+        public bool BolsaOrquestral()
+        {
+            return (NivelHabilidade >= 3) || (HorasPraticaSemanal > 8);
+        }
+    }
+}       
--- Atv-ModaoDoAmanha/Program.cs
+++ Atv-ModaoDoAmanha/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Atv_ModaoDoAmanha;
+
+Console.WriteLine("---Sistema de gestão de música---");
+Console.WriteLine();
+
+AlunoMusica aluno = new AlunoMusica();
+
+
+Console.WriteLine("Seu nome: ");
+aluno.Nome = Console.ReadLine();
+
+Console.WriteLine("Sua mensalidade base: ");
+aluno.MensalidadeBase = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Suas horas de pratica semanal: ");
+aluno.HorasPraticaSemanal = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite seu nível de habilidade: ");
+aluno.NivelHabilidade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\n-- Desempenho do aluno");
+Console.WriteLine($"Nome; {aluno.Nome}");
+Console.WriteLine($"Desconto de horas: {aluno.DescontoHoras()}");
+Console.WriteLine($"Seu nível: {aluno.NivelHabilidade}");
+Console.WriteLine($"Para você ir para o próximo nível: {aluno.HorasFaltantesProximoNivel()}");
+Console.WriteLine($"Sua mensalidade final: {aluno.MensalidadeFinal()}");
+Console.WriteLine($"Vai receber a bolsa orquestral?: {aluno.BolsaOrquestral()}");
